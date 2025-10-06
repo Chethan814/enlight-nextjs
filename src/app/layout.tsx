d031fba044ebb106f6ec0dfae5f0ed7a618dcf73
chethan <chethanchethu99339@gmail.com>
2025-10-06 23:12:23 +0530

    finding the error in css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,12 @@ import "./globals.css";
 import '../../public/assets/css/style.css'
 import '../../public/assets/css/animate.min.css'
 import '../../public/assets/css/aos.min.css'
-import '../../public/assets/css/bootstrap.min.css'
+// import '../../public/assets/css/bootstrap.min.css'
 import '../../public/assets/css/icofont.min.css'
 import '../../public/assets/css/magnific-popup.css'
 import '../../public/assets/css/slick.css'
 import '../../public/assets/css/swiper-bundle.min.css'
-// import '../../public/css/app.css'
+import '../../public/css/app.css'
 
 import Head from 'next/head';
 import Wrapper from './components/layout/mainLayout/wrapper'    
