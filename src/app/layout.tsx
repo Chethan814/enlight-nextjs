c75d29665e73f7c724192ee106ae718dc329dfdb
chethan <chethanchethu99339@gmail.com>
2025-10-06 23:09:36 +0530

    finding the error in css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import '../../public/assets/css/style.css'
 import '../../public/assets/css/animate.min.css'
 import '../../public/assets/css/aos.min.css'
-// import '../../public/assets/css/bootstrap.min.css'
+import '../../public/assets/css/bootstrap.min.css'
 import '../../public/assets/css/icofont.min.css'
 import '../../public/assets/css/magnific-popup.css'
 import '../../public/assets/css/slick.css'
